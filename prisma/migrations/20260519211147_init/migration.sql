9640d6dd36dc0c794233c3ff5b6ed96780f75d50
Jesus24-Dev <siritjesus24@gmail.com>
2026-05-19 17:21:58 -0400

    first commit

---
+++ prisma/migrations/20260519211147_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('ATHLETE', 'COACH', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Roles"[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
