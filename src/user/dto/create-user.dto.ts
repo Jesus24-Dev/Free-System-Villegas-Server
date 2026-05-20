902136c4692f24198b58a01c142d357b23cd44c9
Jesus24-Dev <siritjesus24@gmail.com>
2026-05-20 16:34:25 -0400

    Enhance CreateUserDto validation messages and structure for clarity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,14 +1,33 @@
 import { Roles } from 'src/generated/prisma/enums';
-import { IsArray, IsEmail, IsEnum, IsStrongPassword } from 'class-validator';
+import {
+  IsEmail,
+  IsStrongPassword,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+} from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'El correo electrónico proporcionado no es válido' })
+  @IsNotEmpty({ message: 'El correo electrónico es obligatorio' })
   email: string;
 
-  @IsStrongPassword({minLength: 8, minUppercase: 1, minSymbols: 1})
+  @IsStrongPassword(
+    { minLength: 8, minUppercase: 1, minSymbols: 1 },
+    {
+      message:
+        'La contraseña es demasiado débil. Debe tener al menos 8 caracteres, 1 letra mayúscula y 1 símbolo especial',
+    },
+  )
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
   password: string;
 
-  @IsArray()
-  @IsEnum(Roles, {each: true})
+  @IsArray({ message: 'Los roles deben enviarse dentro de un arreglo ([])' })
+  @IsEnum(Roles, {
+    each: true,
+    message:
+      'Uno o más roles seleccionados no son válidos. Los valores permitidos son: ATHLETE, COACH, ADMIN',
+  })
+  @IsNotEmpty({ message: 'Debes asignar al menos un rol al usuario' })
   role: Roles[];
 }
