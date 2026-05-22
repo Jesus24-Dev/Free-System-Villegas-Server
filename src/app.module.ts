49bccecd546fe540850f241d5b5356eba1d1fd8f
Jesus24-Dev <siritjesus24@gmail.com>
2026-05-22 10:41:44 -0400

    Add PersonModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { UserInfoModule } from './user-info/user-info.module';
 import { WeightsModule } from './weights/weights.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, UserInfoModule, WeightsModule],
+  imports: [PrismaModule, UserModule, UserInfoModule, WeightsModule, PersonModule],
   controllers: [],
   providers: [],
 })
