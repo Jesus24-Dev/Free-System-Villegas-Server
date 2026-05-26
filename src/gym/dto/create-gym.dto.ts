0285a19031e093fa20730417857b925d332201bb
Jesus24-Dev <siritjesus24@gmail.com>
2026-05-26 16:23:23 -0400

    Add gym module with controller, service, DTOs, and entity for managing gym data

---
+++ src/gym/dto/create-gym.dto.ts
@@ -0,0 +1,21 @@
+import { IsString, IsNotEmpty, Length, IsEnum } from 'class-validator';
+import { States } from 'src/generated/prisma/enums';
+
+export class CreateGymDto {
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  @Length(2, 50, { message: 'El nombre debe tener entre 2 y 50 caracteres' })
+  name!: string;
+
+  @IsString({ message: 'La direccion debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'La direccion es obligatoria' })
+  @Length(2, 50, { message: 'La direccion debe tener entre 2 y 50 caracteres' })
+  address!: string;
+
+  @IsEnum(States, {
+    each: true,
+    message: `El estado seleccionado no es valido. Los valores permitidos son: ${Object.values(States).join(', ')}`,
+  })
+  @IsNotEmpty({ message: 'Debes asignar un estado al gimnasio' })
+  state!: States;
+}
