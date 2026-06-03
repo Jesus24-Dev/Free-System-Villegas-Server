e3aa5d7cc04d03692faa78c087527fd3d1e904ea
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-03 14:17:11 -0400

    Add UserInfoModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { UserInfoModule } from './user-info/user-info.module';
 import { WeightsModule } from './weights/weights.module';
 import { PersonModule } from './person/person.module';
 import { GymModule } from './gym/gym.module';
+import { AthleteModule } from './athlete/athlete.module';
+import { CoachModule } from './coach/coach.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, UserInfoModule, WeightsModule, PersonModule, GymModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    WeightsModule,
+    PersonModule,
+    GymModule,
+    AthleteModule,
+    CoachModule,
+  ],
   controllers: [],
   providers: [],
 })
