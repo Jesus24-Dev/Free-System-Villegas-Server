87a51a27f3a016862a67356e45fb1e70b64d2847
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-05 10:24:59 -0400

    feat: restructure modules and implement athlete, coach, competition, gym, person, and user features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { WeightsModule } from './weights/weights.module';
-import { PersonModule } from './person/person.module';
-import { GymModule } from './gym/gym.module';
-import { AthleteModule } from './athlete/athlete.module';
-import { CoachModule } from './coach/coach.module';
-import { CompetitionModule } from './competition/competition.module';
+import { UserModule } from './features/user/user.module';
+import { WeightsModule } from './features/weights/weights.module';
+import { PersonModule } from './features/person/person.module';
+import { GymModule } from './features/gym/gym.module';
+import { AthleteModule } from './features/athlete/athlete.module';
+import { CoachModule } from './features/coach/coach.module';
+import { CompetitionModule } from './features/competition/competition.module';
 
 @Module({
   imports: [
