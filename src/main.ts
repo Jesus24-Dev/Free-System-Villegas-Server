8ba5bd6a9712e4fb577bdcfd384cfa9c2c5e2967
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-08 17:37:25 -0400

    feat: integrate Swagger documentation setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,27 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true, // <--- ESTO ES CRUCIAL
-      whitelist: true, // Limpia campos que no estén en el DTO
+      transform: true,
+      whitelist: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Free System Villegas')
+    .setDescription(
+      'Documentación oficial de la API de gestión de usuarios y atletas',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
