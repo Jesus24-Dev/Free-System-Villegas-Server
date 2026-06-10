36692e53292e2b96f80c3ee2711b2cff945a2c64
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-10 10:51:51 -0400

    feat: implement user registration with password hashing and validation

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -2,7 +2,10 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register-auth.dto';
 
+const saltRounds = 10;
 @Injectable()
 export class AuthService {
   constructor(
@@ -12,13 +15,27 @@ export class AuthService {
 
   async signIn(email: string, pass: string): Promise<AuthDto> {
     const user = await this.userService.findByEmail(email);
-    if (user.password !== pass) {
+
+    const isMatch = await bcrypt.compare(pass, user.password);
+    if (!isMatch) {
       throw new UnauthorizedException(
-        'Los campos no coinciden con el registro almacenado.',
+        'La clave no coincide con el registro encontrado.',
       );
     }
     const payload = { sub: user.id, email: user.email, role: user.role };
 
     return { access_token: await this.jwtService.signAsync(payload) };
+  }
+
+  async register(registerDto: RegisterDto): Promise<AuthDto> {
+    const hashedPassword = await bcrypt.hash(registerDto.password, saltRounds);
+
+    const newUser = await this.userService.create({
+      ...registerDto,
+      password: hashedPassword,
+    });
+    const payload = { sub: newUser.id, email: newUser.email };
+    const token = this.jwtService.sign(payload);
+    return { access_token: token };
   }
 }
