94c718e9e251c1b03b116f0844ee2f420b5a0759
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-12 12:17:40 -0400

    feat: implement CreateGymUseCase for gym creation with owner validation and optional owner_id in CreateGymDto

---
+++ src/features/gym/use-cases/create-gym.use-case.ts
@@ -0,0 +1,34 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateGymDto, GymResponseDto } from '../dto/create-gym.dto';
+
+@Injectable()
+export class CreateGymUseCase {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async execute(
+    userId: string,
+    createGym: CreateGymDto,
+  ): Promise<GymResponseDto> {
+    const coach = await this.prisma.coach.findFirst({
+      where: {
+        person: {
+          user: {
+            id: userId,
+          },
+        },
+      },
+    });
+    if (!coach) {
+      throw new ForbiddenException(
+        'Solo los entrenadores pueden crear gimnasios',
+      );
+    }
+    return this.prisma.gym.create({
+      data: {
+        owner_id: coach.id,
+        ...createGym,
+      },
+    });
+  }
+}
