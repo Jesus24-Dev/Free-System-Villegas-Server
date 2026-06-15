890ab10418c862c73a5bcc0cddb3b1f22d2856d2
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-15 17:40:29 -0400

    feat: add use cases for registering and assigning athletes to gyms in Coach module

--- src/features/coach/coach.controller.ts
+++ src/features/coach/coach.controller.ts
@@ -13,11 +13,19 @@ import { CoachService } from './coach.service';
 import { CreateCoachDto, CoachResponseDto } from './dto/create-coach.dto';
 import { UpdateCoachDto } from './dto/update-coach.dto';
 import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
+import { CreatePersonDto } from '../person/dto/create-person.dto';
+import { AthleteResponseDto } from '../athlete/dto/create-athlete.dto';
+import { RegisterAthleteUseCase } from './use-cases/register-athlete.use-case';
+import { AssignAthleteToGymUseCase } from './use-cases/assign-athlete-gym.use-case';
 
 @ApiTags('Coach')
 @Controller('coach')
 export class CoachController {
-  constructor(private readonly coachService: CoachService) {}
+  constructor(
+    private readonly coachService: CoachService,
+    private readonly registerAthleteUseCase: RegisterAthleteUseCase,
+    private readonly assignAthleteToGymUseCase: AssignAthleteToGymUseCase,
+  ) {}
   @ApiOperation({ summary: 'Crear un nuevo coach' })
   @ApiResponse({
     status: 201,
@@ -29,6 +37,34 @@ export class CoachController {
     @Body() createCoachDto: CreateCoachDto,
   ): Promise<CoachResponseDto> {
     return this.coachService.create(createCoachDto);
+  }
+
+  @Post(':idGym/athlete')
+  @ApiOperation({ summary: 'Registrar nuevo atleta en un gimnasio' })
+  @ApiResponse({
+    status: 201,
+    description: 'Atleta registrado en el gym',
+    type: AthleteResponseDto,
+  })
+  async registerAthleteInGym(
+    @Param('idGym') idGym: string,
+    @Body() createPerson: CreatePersonDto,
+  ): Promise<AthleteResponseDto> {
+    return this.registerAthleteUseCase.execute(createPerson, idGym);
+  }
+
+  @Patch(':idGym/athlete/:idAthlete')
+  @ApiOperation({ summary: 'Asignar un atleta existente a un gimnasio' })
+  @ApiResponse({
+    status: 201,
+    description: 'Atleta registrado en el gym',
+    type: AthleteResponseDto,
+  })
+  async assignAthleteInGym(
+    @Param('idGym') idGym: string,
+    @Param('idAthlete') idAthlete: string,
+  ): Promise<AthleteResponseDto> {
+    return this.assignAthleteToGymUseCase.execute(idAthlete, idGym);
   }
 
   @Get()
