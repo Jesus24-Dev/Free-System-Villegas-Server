4e0a331339e963679a4a963580b2d089e2bb68b8
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-20 11:40:01 -0400

    feat: add gym details response DTO and corresponding service method for fetching gym details

---
+++ src/features/gym/dto/response/athlete-details-response.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { PersonDetailsDto } from './person-details-response.dto';
+
+export class AthleteDetailsDto {
+  @ApiProperty({
+    description: 'ID único del atleta',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  id!: string;
+
+  @ApiProperty({
+    type: PersonDetailsDto,
+  })
+  person!: PersonDetailsDto;
+}
