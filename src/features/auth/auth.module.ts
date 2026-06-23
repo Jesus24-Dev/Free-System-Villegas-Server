75c0aa9fd7a1f545142b1a444b1851735523b22a
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-23 09:18:55 -0400

    fix: refactor JWT configuration to use ConfigService and remove constants file

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -3,21 +3,31 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PasswordService } from './services/password.service';
 import { TokenService } from './services/token.service';
 import { RegisterUserUseCase } from './use-cases/register-user.use-case';
 import { LoggerService } from 'src/common/logger/logger.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     PrismaModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
       global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const expireTime = configService.get<string>('JWT_EXPIRES_IN');
+
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            expiresIn: expireTime as any,
+          },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
--- src/features/auth/constants.ts
+++
@@ -1,7 +0,0 @@
-export const jwtConstants = {
-  secret: process.env.JWT_SECRET,
-};
-
-if (!process.env.JWT_SECRET) {
-  throw new Error('JWT_SECRET missing');
-}
