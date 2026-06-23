6d154241f5d29f1278b5d3ddea6376d65f8e7166
Jesus24-Dev <siritjesus24@gmail.com>
2026-06-23 08:21:39 -0400

    feat: implement admin module with controller and service for gym and user management

--- src/features/admin/admin.controller.ts
+++ src/features/admin/admin.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { Public } from 'src/common/decorators/public.decorator';
 import { GymResponseDto, UserResponseDto } from './dto/response';
+import { JwtPayload } from 'src/features/auth/dto/request';
+import { GetUser } from 'src/features/auth/decorators/get-user.decorator';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('admin')
-@Public()
+@Roles('ADMIN')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
@@ -21,5 +23,10 @@ export class AdminController {
   @Patch('/users/:id/status')
   async changeUserStatus(@Param('id') id: string): Promise<void> {
     await this.adminService.changeUserStatus(id);
+  }
+
+  @Get('')
+  test(@GetUser() user: JwtPayload) {
+    return user;
   }
 }
